b3a1e114aa65bbb0059531299e5ccaa1985c0060
Roshish Shrestha <roshish152002@gmail.com>
2022-09-20 01:52:35 +0545

    bloc implementation

--- lib/features/weather_app/presentation/bloc/weather_info_bloc.dart
+++ lib/features/weather_app/presentation/bloc/weather_info_bloc.dart
@@ -1,13 +1,43 @@
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_app/core/error/failures.dart';
+import 'package:weather_app/features/weather_app/domain/entities/weather_info.dart';
+import 'package:weather_app/features/weather_app/domain/usecases/get_weather_info.dart';
 
 part 'weather_info_event.dart';
 part 'weather_info_state.dart';
 
+const String serverFailureMessage = 'Server Failure';
+const String cacheFailureMessage = 'Cache Failure';
+
 class WeatherInfoBloc extends Bloc<WeatherInfoEvent, WeatherInfoState> {
-  WeatherInfoBloc() : super(WeatherInfoInitial()) {
-    on<WeatherInfoEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetWeatherInfo getWeatherInfo;
+
+  WeatherInfoBloc({
+    required this.getWeatherInfo,
+  }) : super(WeatherInfoInitial()) {
+    on<WeatherInfoEvent>((event, emit) async {
+      if (event is GetSearchedWeatherInfo) {
+        emit(WeatherInfoLoading());
+        final failureOrWeatherInfo =
+            await getWeatherInfo(Params(cityName: event.city));
+        emit(failureOrWeatherInfo.fold(
+          (failure) => WeatherInfoError(message: _mapFailureToMessage(failure)),
+          (weatherInfo) => WeatherInfoLoaded(weatherInfo: weatherInfo),
+        ));
+      }
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }
--- lib/features/weather_app/presentation/bloc/weather_info_event.dart
+++ lib/features/weather_app/presentation/bloc/weather_info_event.dart
@@ -1,16 +1,14 @@
 part of 'weather_info_bloc.dart';
 
 abstract class WeatherInfoEvent extends Equatable {
-  const WeatherInfoEvent();
-
   @override
   List<Object> get props => [];
 }
 
-class GetWeatherInfo extends WeatherInfoEvent {
+class GetSearchedWeatherInfo extends WeatherInfoEvent {
   final String city;
 
-  const GetWeatherInfo({required this.city});
+  GetSearchedWeatherInfo({required this.city});
 
   @override
   List<Object> get props => [city];
